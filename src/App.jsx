428fe22d448aaac6192b80a1792b43d004a27c91
e-yanagita-gs <e.yanagita@genbasupport.com>
2025-04-24 14:40:24 +0900

    LocalStorageに保存するよう設定

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 // App.jsx
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import MemoContainer from "./MemoContainer.jsx";
 
 function App() {
-  const [memos, setMemos] = useState([
-    { id: 1, content: "内容1\nこれは1行目です。" },
-    { id: 2, content: "内容2\nこれも1行目です。" },
-  ]);
+  const [memos, setMemos] = useState(() => {
+    const storedMemos = localStorage.getItem("memos");
+    return storedMemos ? JSON.parse(storedMemos) : [];
+  });
+
   const [selectedMemoId, setSelectedMemoId] = useState(null); // State の名前も変更
+
+  useEffect(() => {
+    localStorage.setItem("memos", JSON.stringify(memos));
+  }, [memos]);
 
   const handleMemoClick = (id) => {
     setSelectedMemoId(id);
@@ -19,7 +24,7 @@ function App() {
       <h1>メモアプリ</h1>
       <MemoContainer
         memos={memos}
-        selectedMemoId={selectedMemoId} // props の名前を修正
+        selectedMemoId={selectedMemoId}
         onMemoClick={handleMemoClick}
       />
     </div>
